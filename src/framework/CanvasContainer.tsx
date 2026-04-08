1623d05e1548b28937360aa9e285e5aa67a95d7a
Kael.Cai <saint.archangel.satan@gmail.com>
2026-04-08 20:04:08 +0800

    feat: bypass container div for default props and update transform origin to center

--- src/framework/CanvasContainer.tsx
+++ src/framework/CanvasContainer.tsx
@@ -17,12 +17,17 @@ export const CanvasContainer: React.FC<CanvasProps> = ({
 }) => {
   const isAbsolute = x !== undefined || y !== undefined;
   
+  // If no canvas properties were provided at all, do not wrap in a breaking div
+  if (x === undefined && y === undefined && width === 'auto' && height === 'auto' && className === '' && opacity === 1 && zIndex === 0 && rotation === 0 && scale === 1 && !style && !onClick) {
+    return <>{children}</>;
+  }
+  
   const containerStyle: React.CSSProperties = isAbsolute ? {
     position: 'absolute',
     left: x || 0,
     top: y || 0,
     transform: `scale(${scale}) rotate(${rotation}deg)`,
-    transformOrigin: '0 0',
+    transformOrigin: '50% 50%',
     opacity,
     zIndex,
     width,
